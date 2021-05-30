fea8dbff10b2d7e9761e03a4f4acc6072866d76b
tpjbaralsantosh <santoshbaral258@gmail.com>
2021-05-30 17:03:11 +0545

    tooltip

---
+++ ny/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <div className="tool">
+        <h1 className="main">Ram Thapa</h1>
+        <div className="tooltiptext">
+         <h2 className="text">Ram Thapa</h2>
+         <h2 className="email">ramthapa@gmail.com</h2>
+       </div>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ pagination/src/App.js
@@ -0,0 +1,16 @@
+
+import React, {useState,useEffect} from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+
+    <h1 className="tool">Ram Thapa</h1>
+
+    </div>
+  )
+    
+}
+
+
